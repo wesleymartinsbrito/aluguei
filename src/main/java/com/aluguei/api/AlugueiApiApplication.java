fcd7c72df272b53e4a6d9f3f2c1bb6cae360ab9f
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-10-22 12:04:06 -0300

    First commit

---
+++ src/main/java/com/aluguei/api/AlugueiApiApplication.java
@@ -0,0 +1,13 @@
+package com.aluguei.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugueiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugueiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluguei/api/AlugueiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluguei.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugueiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
